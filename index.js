2715714d95ccdd66225054724915cc37c4322d10
vatimetou ebeker <fatimaabcar2@gmail.com>
2022-01-20 12:47:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+// Write your code here!cinst 
+const main= document.getElementById('main')
+main.remove();
+const newHeader = document.createElement("H1")
+newHeader.id='victory';
+newHeader.textContent="YOUR-NAME is the champion";
